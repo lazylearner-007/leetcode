30669189bb0014b927e30cf631872dc55a1d1914
lazylearner-007 <76438087+lazylearner-007@users.noreply.github.com>
2021-02-12 16:04:57 +0530

    Update 1110leet.py

--- 1110leet.py
+++ 1110leet.py
@@ -1,79 +1,39 @@
+class Solution:
+	def delNodes(self, root: TreeNode, to_delete):
+		self.to_delete = self.listToDict(to_delete)
+		self.result = []
 
-class node(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+		if not self.to_delete.get(root.val):
+			self.result.append(root)
 
-class Solution(object):
-	def delNodes(self,root,to_delete):
-		
-		self.to_delete = self.list_to_dict(to_delete)
-		self.result = [root]
-		
-		#if root to be deleted
-		exception = self.check_exception(root)
-
-		while self.to_delete:
-			self.recursion(self.result[0])
-
-		#delete root node from result array
-		if exception:
-			self.result.pop(0)
-
+		self.traverseNodes(root,parent=None)
+        
 		return self.result
-		
-	def recursion(self,root):
-		if not root :
+
+	def traverseNodes(self,root,parent):
+		if not root:
 			return
 
-		#postorder traversal
-		root.left = self.recursion(root.left)
-		root.right = self.recursion(root.right)
+		self.traverseNodes(root.left,root)
+		self.traverseNodes(root.right,root)
 
-		try:
-			#check condition, current node value present in to_delete dictionary
-			self.to_delete[root.val]
-
-			#add left and right nodes to the result array as seperate roots
-			if root.left :
+		if self.to_delete.get(root.val):
+			if root.left and root.left.val:
 				self.result.append(root.left)
-								
-			if root.right :
+			if root.right and root.right.val:
 				self.result.append(root.right)
-				
-			self.to_delete.pop(root.val)
-			return None
-
-		except:
-			pass
-
-		return root
-
-	
-	def list_to_dict(self,to_delete):
+			if parent:
+				if parent.left and parent.left.val == root.val:
+					parent.left = None
+				elif parent.right and parent.right.val == root.val:
+					parent.right = None
+		
+	def listToDict(self,to_delete):
 		Dict = {}
 		for i in to_delete:
 			Dict[i]=i
 		return Dict
 
-	def check_exception(self,root):
-		try:
-			self.to_delete[root.val]
-			return True
-		except:
-			return False
 
-#sample test case       
-root = node(1)
-root.left = node(2)
-root.right= node(3)
-root.left.left = node(4)
-root.left.right = node(5)
-root.right.left = node(6)
-root.right.right = node(7)
 
-d = [1,6]
-s = Solution()
-print(s.delNodes(root,d))
 		
